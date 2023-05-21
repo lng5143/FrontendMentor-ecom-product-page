a8c357be970fbbeaa3a3c8a0dfc8b43a53580885
lamnguyen <nguyenphuonglam96@gmail.com>
2023-05-21 22:48:29 +0700

    openMobileMenu, closeMobileMenu, openClickImage, closeClickImage

--- script.js
+++ script.js
@@ -1,7 +1,13 @@
+const overlay = document.querySelector('#overlay');
+const navElements = document.querySelector('#nav-elements');
+const imageClick = document.querySelector('#image-click');
+
 const menuIcon = document.querySelector('#icon-menu');
-const closeIcon = document.querySelector('#menu-close');
+const closeIconMenu = document.querySelector('#menu-close');
 const mainNext = document.querySelector('#main-image-next');
 const mainPrevious = document.querySelector('#main-image-previous');
+const mainImage = document.querySelector('#main-image');
+const closeIconClickImage = document.querySelector('#image-click-close');
 const clickNext = document.querySelector('#image-click-next');
 const clickPrevious = document.querySelector('#image-click-previous');
 const mainThumbnails = document.querySelectorAll('.product-thumbnail');
@@ -10,14 +16,18 @@ const plus = document.querySelector('#plus');
 const minus = document.querySelector('#minus');
 const addCart = document.querySelector('#add-to-cart');
 
+const mediaQuery = window.matchMedia('all and (min-width: 565px)');
+
 menuIcon.addEventListener('click', openMobileMenu);
-closeIcon.addEventListener('click', closeMobileMenu);
+closeIconMenu.addEventListener('click', closeMobileMenu);
 mainNext.addEventListener('click', nextMainImage);
 mainPrevious.addEventListener('click', prevMainImage);
+mainImage.addEventListener('click', openClickImage);
+closeIconClickImage.addEventListener('click', closeClickImage);
 clickNext.addEventListener('click', nextClickImage);
 clickPrevious.addEventListener('click', prevClickImage);
-mainThumbnails.addEventListener('click', chooseMainImageByThumbnail);
-clickThumbnails.addEventListener('click', chooseClickImageByThumbnail);
+// mainThumbnails.addEventListener('click', chooseMainImageByThumbnail);
+// clickThumbnails.addEventListener('click', chooseClickImageByThumbnail);
 plus.addEventListener('click', increaseQuantity);
 minus.addEventListener('click', decreaseQuantity);
 addCart.addEventListener('click', addCart);
@@ -25,11 +35,13 @@ addCart.addEventListener('click', addCart);
 
 
 function openMobileMenu() {
-
+    overlay.style.display = 'block';
+    navElements.style.display = 'block';
 }
 
 function closeMobileMenu() {
-
+    overlay.style.display = 'none';
+    navElements.style.display = 'none';
 }
 
 function nextMainImage() {
@@ -38,6 +50,19 @@ function nextMainImage() {
 
 function prevMainImage() {
 
+}
+
+function openClickImage() {
+    if (mediaQuery.matches) {
+        overlay.style.display = 'block';
+        imageClick.style.display = 'flex';
+        console.log('open click iamge');
+    }
+}
+
+function closeClickImage() {
+    overlay.style.display = 'none';
+    imageClick.style.display = 'none';
 }
 
 function nextClickImage() {
@@ -64,6 +89,6 @@ function decreaseQuantity() {
 
 }
 
-function addCart() {
+function addToCart() {
 
 }
