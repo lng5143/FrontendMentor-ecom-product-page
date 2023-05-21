52e7acfe83b732f08b7df803cb755520ea1f3550
lamnguyen <nguyenphuonglam96@gmail.com>
2023-05-21 22:02:05 +0700

    javascript skeleton

---
+++ script.js
@@ -0,0 +1,69 @@
+const menuIcon = document.querySelector('#icon-menu');
+const closeIcon = document.querySelector('#menu-close');
+const mainNext = document.querySelector('#main-image-next');
+const mainPrevious = document.querySelector('#main-image-previous');
+const clickNext = document.querySelector('#image-click-next');
+const clickPrevious = document.querySelector('#image-click-previous');
+const mainThumbnails = document.querySelectorAll('.product-thumbnail');
+const clickThumbnails = document.querySelectorAll('.image-click-thumbnail');
+const plus = document.querySelector('#plus');
+const minus = document.querySelector('#minus');
+const addCart = document.querySelector('#add-to-cart');
+
+menuIcon.addEventListener('click', openMobileMenu);
+closeIcon.addEventListener('click', closeMobileMenu);
+mainNext.addEventListener('click', nextMainImage);
+mainPrevious.addEventListener('click', prevMainImage);
+clickNext.addEventListener('click', nextClickImage);
+clickPrevious.addEventListener('click', prevClickImage);
+mainThumbnails.addEventListener('click', chooseMainImageByThumbnail);
+clickThumbnails.addEventListener('click', chooseClickImageByThumbnail);
+plus.addEventListener('click', increaseQuantity);
+minus.addEventListener('click', decreaseQuantity);
+addCart.addEventListener('click', addCart);
+
+
+
+function openMobileMenu() {
+
+}
+
+function closeMobileMenu() {
+
+}
+
+function nextMainImage() {
+
+}
+
+function prevMainImage() {
+
+}
+
+function nextClickImage() {
+
+}
+
+function prevClickImage() {
+
+}
+
+function chooseMainImageByThumbnail() {
+
+}
+
+function chooseClickImageByThumbnail() {
+
+}
+
+function increaseQuantity() {
+
+}
+
+function decreaseQuantity() {
+
+}
+
+function addCart() {
+
+}
